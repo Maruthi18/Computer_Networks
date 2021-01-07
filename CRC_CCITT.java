22ce645e0c0528d7c68a9a63176de0688a5976e3
Maruthi18 <syavemaruthi18@gmail.com>
2021-01-08 03:19:47 +0530

    updated

--- CRC_CCITT.java
+++ CRC_CCITT.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 
 // length = Size of an array
-// length = Number of character in a string
+// length() = Number of character in a string
 
 public class CRC_CCITT {
     public static void main(String[] args) {

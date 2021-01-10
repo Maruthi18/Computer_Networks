93d263840400027698205a19fbdac6d1b3d47f34
Maruthi18 <syavemaruthi18@gmail.com>
2021-01-10 23:37:21 +0530

    Explaination done

--- Tcp Socket Progrmming/TCPS.java
+++ Tcp Socket Progrmming/TCPS.java
@@ -4,21 +4,30 @@ import java.io.*;
 @SuppressWarnings("ALL")
 public class TCPS {
     public static void main (String[] args) throws Exception {
+
+        // portNumber must be same in client & Server side to contact
         ServerSocket sersock = new ServerSocket(4000);
+
         System.out.println("Server ready for connection");
 
+        // Every  Single Client Request We Create a One socket for the Client , and create a socket object
         Socket sock = sersock.accept();
 
         System.out.println("Connection Is successful and waiting for chatting");
 
+        // Here We get a file name , file will get from port
         InputStream istream = sock.getInputStream();
 
+        // Read the file from Socekt Input port
         BufferedReader fileRead = new BufferedReader(new InputStreamReader(istream));
 
+        // Stores the buffer Reader information in a file (fname Variable)
         String fname = fileRead.readLine();
 
+        // Here We are Reading a contents of the file (fname)
         BufferedReader ContentRead = new BufferedReader(new FileReader(fname));
 
+        // Here sending it out to client
         OutputStream ostream = sock.getOutputStream();
 
         PrintWriter pwrite = new PrintWriter(ostream, true);

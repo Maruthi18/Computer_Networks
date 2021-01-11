d77117a8d5d563b11a0408dd5945a21378908cd9
Maruthi18 <syavemaruthi18@gmail.com>
2021-01-11 14:25:42 +0530

    Changes Updated

--- CRC_CCITT/CRC_CCITT.java
+++ CRC_CCITT/CRC_CCITT.java
@@ -4,39 +4,38 @@ import java.util.Scanner;
 import java.util.*;
 
 
-
 // length = Size of an array
 // length() = Number of character in a string
 
 public class CRC_CCITT {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+    public static void main (String[] args) {
+        Scanner s = new Scanner (System.in);
 
         // ----------- INPUT DATA ---------
 
-        System.out.print(" Enter The Message Bits : ");
-        String message = s.nextLine();
-        System.out.print(" Enter Generator : ");
-        String generator = s.nextLine();
+        System.out.print (" Enter The Message Bits : ");
+        String message = s.nextLine ();
+        System.out.print (" Enter Generator : ");
+        String generator = s.nextLine ();
 
-        int data[] = new int[message.length() + generator.length() - 1];
-        int divisor[] = new int[generator.length()];
+        int data[] = new int[ message.length () + generator.length () - 1 ];
+        int divisor[] = new int[ generator.length () ];
 
-        for (int i=0; i<message.length(); i++) {
-            data[i] = Integer.parseInt(message.charAt(i) + "");
+        for ( int i = 0; i < message.length (); i++ ) {
+            data[ i ] = Integer.parseInt (message.charAt (i) + "");
         }
 
-        for (int i=0; i<generator.length(); i++) {
-            divisor[i] = Integer.parseInt(generator.charAt(i) + "");
+        for ( int i = 0; i < generator.length (); i++ ) {
+            divisor[ i ] = Integer.parseInt (generator.charAt (i) + "");
 
         }
 
         // ----- CALCULATION OF CRC -----------
 
-        for (int i=0; i<message.length();i++) {
-            if(data[i] == 1){
-                for (int j=0; j<divisor.length; j++) {
-                    data[ i+j ] ^= divisor[ j ];          // here the result stored back to data only .....
+        for ( int i = 0; i < message.length (); i++ ) {
+            if ( data[ i ] == 1 ) {
+                for ( int j = 0; j < divisor.length; j++ ) {
+                    data[ i + j ] ^= divisor[ j ];          // here the result stored back to data only .....
 
                 }
             }
@@ -44,39 +43,39 @@ public class CRC_CCITT {
 
         // ------- DISPLAY CRC ---------------
 
-        System.out.print(" The Checksum Code : ");
-        for (int i=0; i<message.length(); i++) {
-            data[i] = Integer.parseInt(message.charAt(i) + "");   // This is Remainder of the Sender Side
+        System.out.print (" The Checksum Code : ");
+        for ( int i = 0; i < message.length (); i++ ) {
+            data[ i ] = Integer.parseInt (message.charAt (i) + "");   // This is Remainder of the Sender Side
         }
-        for (int i=0; i< data.length; i++){
-            System.out.print( data[i] );
+        for ( int i = 0; i < data.length; i++ ) {
+            System.out.print (data[ i ]);
         }
-        System.out.println();
+        System.out.println ();
 
         // ---------- CHECK FOR INPUT CRC CODE -------------
 
-        System.out.print(" Enter The Checksum code : ");
-            message = s.nextLine();
-        System.out.print(" Enter Generator : ");
-            generator = s.nextLine();
+        System.out.print (" Enter The Checksum code : ");
+        message = s.nextLine ();
+        System.out.print (" Enter Generator : ");
+        generator = s.nextLine ();
 
-         data = new int[message.length() + generator.length() - 1];
-         divisor = new int[generator.length()];
+        data = new int[ message.length () + generator.length () - 1 ];
+        divisor = new int[ generator.length () ];
 
-        for (int i=0; i<message.length(); i++) {
-            data[i] = Integer.parseInt(message.charAt(i) + "");
+        for ( int i = 0; i < message.length (); i++ ) {
+            data[ i ] = Integer.parseInt (message.charAt (i) + "");
         }
 
-        for (int i=0; i<generator.length(); i++) {
-            divisor[i] = Integer.parseInt(generator.charAt(i) + "");
+        for ( int i = 0; i < generator.length (); i++ ) {
+            divisor[ i ] = Integer.parseInt (generator.charAt (i) + "");
         }
 
         // -------- CALCULATION OF REMAINDER -----------
 
-        for (int i=0; i<message.length();i++) {
-            if(data[i] == 1){
-                for (int j=0; j<divisor.length; j++) {
-                    data[ i+j] ^= divisor[j];
+        for ( int i = 0; i < message.length (); i++ ) {
+            if ( data[ i ] == 1 ) {
+                for ( int j = 0; j < divisor.length; j++ ) {
+                    data[ i + j ] ^= divisor[ j ];
 
                 }
             }
@@ -85,18 +84,18 @@ public class CRC_CCITT {
         // --------- DISPLAY VALIDITY OF DATA -------------
 
         boolean valid = true;
-        for (int i = 0; i < data.length; i++) {
-            if ( data[i] == 1 ) {
+        for ( int i = 0; i < data.length; i++ ) {
+            if ( data[ i ] == 1 ) {
                 valid = false;
                 break;
             }
         }
 
         if ( valid == true ) {
-            System.out.println(" VALID ");
+            System.out.println (" VALID ");
         }
         else {
-            System.out.println(" CRC ERROR OCCURS ");
+            System.out.println (" CRC ERROR OCCURS ");
         }
     }
 }
